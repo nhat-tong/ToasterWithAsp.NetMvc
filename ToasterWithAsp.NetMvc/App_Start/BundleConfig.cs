4e4ae401fe38866df5c96e196ff32614e486d30e
Hoang Quoc Nhat TONG <thqnhat@gmail.Com>
2015-06-20 12:46:15 +0200

    add toaster js to project

--- ToasterWithAsp.NetMvc/App_Start/BundleConfig.cs
+++ ToasterWithAsp.NetMvc/App_Start/BundleConfig.cs
@@ -21,11 +21,13 @@ namespace ToasterWithAsp.NetMvc
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                      "~/Scripts/respond.js",
+                      "~/Scripts/toastr.min.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/toastr.min.css"));
         }
     }
 }
